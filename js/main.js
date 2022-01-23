e535a0d9b4a303e120639eb0c75ff41f030b3b7f
gr8bek <gr8.nazarov@mail.ru>
2022-01-23 18:15:48 +0500

    Finish Pokemon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var elPokemonForm = document.querySelector(".pokemon--form");
+var elPokemonForm = document.querySelector(".js-pokemon-form");
 var elPokemonInputURL = document.querySelector(".pokemon__input--url");
 var elPokemonInputName = document.querySelector(".pokemon__input--name");
 var elPokemonInputType = document.querySelector(".pokemon__input--type");
@@ -6,78 +6,71 @@ var elPokemonInputHeight = document.querySelector(".pokemon__input--height");
 var elPokemonInputWeight = document.querySelector(".pokemon__input--weight");
 var elPokemonsList = document.querySelector(".pokemons__list");
 
-function addNewPokemon(url, name, type, height, weight, pokemonList) {
-  var newPokemonUrl = url.value.trim();
-  var newPokemonName = name.value.trim();
-  var newPokemonType = type.value.trim();
-  var newPokemonHeight = height.value.trim();
-  var newPokemonWeight = weight.value.trim();
+renderPokemons();
 
-  var newPokemon = {
-    img: newPokemonUrl,
-    name: newPokemonName,
-    type: newPokemonType,
-    height: newPokemonHeight,
-    weight: newPokemonWeight,
-  };
-
-  pokemonList.unshift(newPokemon);
-}
-
-function handleFormSubmit(evt) {
+elPokemonForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  addNewPokemon(
-    elPokemonInputURL,
-    elPokemonInputName,
-    elPokemonInputType,
-    elPokemonInputHeight,
-    elPokemonInputWeight,
-    pokemons
-  );
+  const pokemon = getFormValues(elPokemonForm);
 
-  renderPokemons(pokemons);
+  const newPokemon = addNewPokemon(pokemon);
+
+  const singleHTML = getHTML(newPokemon);
+
+  elPokemonsList.insertAdjacentHTML("afterbegin", singleHTML);
+
+  elPokemonForm.reset();
+});
+
+function addNewPokemon(pokemon) {
+  // Add new object
+  var newPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    type: pokemon.type.split(","),
+    height: `${pokemon.height / 100} m`,
+    weight: `${pokemon.weight} kg`,
+  };
+
+  pokemons.unshift(newPokemon);
+
+  return newPokemon;
 }
-elPokemonForm.addEventListener("submit", handleFormSubmit);
 
-function renderPokemons(_pokemons, element) {
-  element.innerHTML = null;
-  for (var i = 0; i < _pokemons.length; i++) {
-    // Creating Elements
-    var newLi = document.createElement("li");
-    var newImg = document.createElement("img");
-    var newHeading = document.createElement("h3");
-    var newType = document.createElement("p");
-    var newHeight = document.createElement("p");
-    var newWeight = document.createElement("p");
-
-    // Set Attributes
-    newLi.setAttribute("class", "pokemon");
-    newLi.setAttribute("class", "");
-    newImg.setAttribute("src", _pokemons[i].img);
-    newImg.setAttribute("alt", _pokemons[i].name + "poster");
-    newImg.setAttribute("width", 157);
-    newImg.setAttribute("height", 157);
-    newHeading.setAttribute("class", "pokemon__name");
-    newType.setAttribute("class", "pokemon__type");
-    newHeight.setAttribute("class", "pokemon__height");
-    newWeight.setAttribute("class", "pokemon__weight");
-
-    // Assign values
-    newHeading.textContent = _pokemons[i].name;
-    newType.textContent = _pokemons[i].type;
-    newHeight.textContent = _pokemons[i].height;
-    newWeight.textContent = _pokemons[i].weight;
-
-    // Append
-    newLi.appendChild(newImg);
-    newLi.appendChild(newHeading);
-    newLi.appendChild(newType);
-    newLi.appendChild(newHeight);
-    newLi.appendChild(newWeight);
-
-    elPokemonsList.appendChild(newLi);
+function renderPokemons() {
+  var html = "";
+  for (var i = 0; i < pokemons.length; i++) {
+    html += getHTML(pokemons[i]);
   }
+  elPokemonsList.innerHTML = html;
 }
 
-renderPokemons(pokemons, elPokemonsList);
+function getHTML(pokemon) {
+  // Rendering Pokemon
+  return `<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+  <div class="card h-100 custom-card">
+    <div class="img-wrapper">
+      <img width="157" height="157" src="https://www.serebii.net/art/th/${
+        pokemon.id
+      }.png" loading="lazy" class="card-img-top mx-auto" alt="${pokemon.name}">
+    </div>
+    <div class="card-body">
+      <h4 class="card-title fw-bold">${pokemon.name}</h4>
+      <p class="fw-bolder">${pokemon.type.join(", ")}</p>
+      <strong class="me-4">${pokemon.weight}</strong>
+      <strong>${pokemon.height}</strong>
+    </div>
+  </div>
+</div>`;
+}
+
+function getFormValues(elForm) {
+  const elsInput = elForm.querySelectorAll("input[name]");
+  const pokemon = {};
+
+  elsInput.forEach((el) => {
+    pokemon[el.name] = el.value;
+  });
+
+  return pokemon;
+}
